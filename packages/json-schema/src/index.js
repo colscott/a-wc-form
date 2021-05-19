428c482f7390f5c267d4f68b1e094bb8c8ef7119
colin <colinscott1@gmail.com>
2021-05-19 19:54:33 -0400

    chore: remove invalid exports

--- packages/json-schema/src/index.js
+++ packages/json-schema/src/index.js
@@ -2,6 +2,4 @@ import "./components/json-schema-form.js";
 import "./components/json-schema-control.js";
 
 export { controlBinder, controlBinders } from "a-wc-form-binder";
-export { getSchema, getValue } from "./lib/json-schema-data-binder.js";
-export { isRequired } from "./templates/controls.js";
-export { controlTemplates, uiTemplates as layoutTemplates } from "./lib/template-registry.js";
+export { getLayout } from "./lib/layout-generator";
