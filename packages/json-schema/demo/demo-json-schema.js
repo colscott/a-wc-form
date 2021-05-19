11eab33528a313b7c980b24150cb1e6513cda3a8
colin <colinscott1@gmail.com>
2021-05-19 19:53:55 -0400

    chore: move demo to component

--- packages/json-schema/demo/demo-json-schema.js
+++ packages/json-schema/demo/demo-json-schema.js
@@ -4,6 +4,7 @@ import {
   controlBinder as binder,
   controlBinders as binders
 } from "a-wc-form-binder";
+import "a-wc-form-layout";
 import { schema, layout } from "./mock.js";
 import "../src/components/json-schema-form.js";
 import "../src/components/json-schema-control.js";
@@ -43,55 +44,79 @@ customElements.define(
     render() {
       return html`
         <section class="examples">
-          <h2>json-schema-form</h2>
+          <h2>json-schema-control</h2>
           <br />
-          <json-schema-form
+          <form-binder
             @form-binder:change=${e => {
               this.data = e.detail.data;
               this.requestUpdate();
             }}
             .data=${this.data}
-            .schema=${schema}
-          ></json-schema-form
-          ><br />
-
-          <h2>json-schema-form</h2>
-          <br />
-          <json-schema-form
-            @form-binder:change=${e => {
-              this.data = e.detail.data;
-              this.requestUpdate();
-            }}
-            .data=${this.data}
-            .schema=${schema}
-            .layout=${layout}
-          ></json-schema-form
-          ><br />
-
-          <h2>json-schema-form</h2>
-          <br />
-          <json-schema-form
-            @form-binder:change=${e => {
-              this.data = e.detail.data;
-              this.requestUpdate();
-            }}
-            .data=${this.data}
-            .schema=${schema}
           >
-            <h2>json-schema-control</h2>
-            <br />
-            <json-schema-control ref="/name"></json-schema-control>
-            <json-schema-control ref="/vegetarian"></json-schema-control>
-            <json-schema-control ref="/postalCode"></json-schema-control>
-            <json-schema-control ref="/personalData"></json-schema-control>
-            <json-schema-control ref="/personalData/age"></json-schema-control>
-            <json-schema-control ref="/comments"></json-schema-control>
-            <json-schema-control ref="/comments/1/date"></json-schema-control>
-            <json-schema-control ref="/telephoneNumbers"></json-schema-control>
             <json-schema-control
+              .schema=${schema}
+              ref="#"
+            ></json-schema-control> </form-binder
+          ><br />
+
+          <h2>json-schema-form</h2>
+          <br />
+          <form-layout
+            @form-binder:change=${e => {
+              this.data = e.detail.data;
+              this.requestUpdate();
+            }}
+            .data=${this.data}
+            .layout=${layout}
+          ></form-layout
+          ><br />
+
+          <h2>json-schema-control</h2>
+          <br />
+          <form-binder
+            @form-binder:change=${e => {
+              this.data = e.detail.data;
+              this.requestUpdate();
+            }}
+            .data=${this.data}
+          >
+            <json-schema-control
+              .schema=${schema}
+              ref="/name"
+            ></json-schema-control>
+            <json-schema-control
+              .schema=${schema}
+              ref="/vegetarian"
+            ></json-schema-control>
+            <json-schema-control
+              .schema=${schema}
+              ref="/postalCode"
+            ></json-schema-control>
+            <json-schema-control
+              .schema=${schema}
+              ref="/personalData"
+            ></json-schema-control>
+            <json-schema-control
+              .schema=${schema}
+              ref="/personalData/age"
+            ></json-schema-control>
+            <json-schema-control
+              .schema=${schema}
+              ref="/comments"
+            ></json-schema-control>
+            <json-schema-control
+              .schema=${schema}
+              ref="/comments/1/date"
+            ></json-schema-control>
+            <json-schema-control
+              .schema=${schema}
+              ref="/telephoneNumbers"
+            ></json-schema-control>
+            <json-schema-control
+              .schema=${schema}
               ref="/telephoneNumbers/1"
             ></json-schema-control>
-          </json-schema-form>
+          </form-binder>
         </section>
         <section class="data">
           <pre>${JSON.stringify(this.data, null, 2)}</pre>
