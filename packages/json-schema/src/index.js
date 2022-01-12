7e90dd3aa7b151d19f8a8e5a3446df830d7e6fe8
colinrscott <colin.scott@trapezegroup.com>
2022-01-12 17:00:49 -0500

    chore: add json schema index file

---
+++ packages/json-schema/src/index.js
@@ -0,0 +1,3 @@
+export * as jsonSchemaControl from './components/json-schema-control.js';
+export * as jsonSchemaForm from './components/json-schema-form.js';
+export * as layoutGenerator from './lib/layout-generator.js';
